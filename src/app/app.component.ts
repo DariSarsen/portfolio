7eeff78b31267a0921a720a6aa8f54f14554f99b
daris <darisarsen24@gmail.com>
2024-03-15 05:56:47 +0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule} from '@angular/common';
+import { NavbarComponent} from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Darina Sarsenova';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {AboutMeComponent} from './components/about-me/about-me.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {ProjectsComponent} from './components/projects/projects.component';
+import {ResumeComponent} from './components/resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AboutMeComponent, title: 'Darina Sarsenova' },
+    { path: 'contact', component: ContactComponent, title: 'Contact with me' },
+    { path: 'projects', component: ProjectsComponent, title: 'Projects' },
+    { path: 'resume', component: ResumeComponent, title: 'Resume' },
+    { path: '**', component: AboutMeComponent, title: 'Darina Sarsenova' }, //PageNotFoundComponent
+];
